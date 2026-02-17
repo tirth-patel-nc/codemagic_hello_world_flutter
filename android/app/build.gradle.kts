f8131d9a8d6bf5efebd1bc85a6fd2cb378493ec5
tirth-patel-nc <tirth@nevercode.io>
2026-02-17 18:20:36 +0530

    com.exam

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,7 +15,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.codemagic_hello_world_flutter"
+    namespace = "com.exam.codemagic_hello_world_flutter"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -30,7 +30,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.codemagic_hello_world_flutter"
+        applicationId = "com.exam.codemagic_hello_world_flutter"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/example/codemagic_hello_world_flutter/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/codemagic_hello_world_flutter/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.codemagic_hello_world_flutter
+package com.exam.codemagic_hello_world_flutter
 
 import io.flutter.embedding.android.FlutterActivity
 
